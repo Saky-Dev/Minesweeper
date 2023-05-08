b632669a80d2e2496f6e43cc83d9da789a695ef9
Saky-Dev <garciaisaac468@gmail.com>
2023-05-08 16:21:38 -0600

    Now defficulty selector is working with JS

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,86 @@
 const handleButtonChangeTheme = () => document.body.classList.toggle(string_values.DARK_THEME)
+const handleButtonSlrOptionsVisibility = () => selector.classList.toggle(string_values.VISIBILITY_STATUS)
+const handleButtonSlrOptions = e => {
+  const key = e.target.getAttribute('key')
+
+  difficulty_selected = key
+  text_difficulty_selected.innerText = game_prperties[key].text
+  selector.classList.toggle(string_values.VISIBILITY_STATUS)
+}
 
 const string_values = {
-  DARK_THEME: 'dark'
+  DARK_THEME: 'dark',
+  VISIBILITY_STATUS: 'visible',
+  DIFFICULTY: {
+    EASY: 'easy',
+    NORMAL: 'normal',
+    HARD: 'hard',
+    EXTREME: 'extreme'
+  }
+}
+
+const game_prperties = {
+  [string_values.DIFFICULTY.EASY]: {
+    text: 'Easy',
+    mines: 10,
+    size: {
+      cols: 10,
+      rows: 10
+    }
+  },
+  [string_values.DIFFICULTY.NORMAL]: {
+    text: 'Normal',
+    mines: 50,
+    size: {
+      cols: 18,
+      rows: 12
+    }
+  },
+  [string_values.DIFFICULTY.HARD]: {
+    text: 'Hard',
+    mines: 100,
+    size: {
+      cols: 30,
+      rows: 20
+    }
+  },
+  [string_values.DIFFICULTY.EXTREME]: {
+    text: 'Extreme',
+    mines: 200,
+    size: {
+      cols: 40,
+      rows: 25
+    }
+  }
 }
 
 Object.freeze(string_values)
+Object.freeze(game_prperties)
 
+const selector = document.querySelector('div.selector')
+const slr_options = document.querySelector('ul.slr-options')
+const text_difficulty_selected = document.querySelector('span#difficulty-selected')
 const button_change_theme = document.getElementById('change-theme')
+const button_slr_options_visibility = document.querySelector('button.slr-options-visibility')
 
 const prefered_color = window.matchMedia(`(prefers-color-scheme: ${string_values.DARK_THEME})`)
+
+let difficulty_selected = undefined
 
 if (prefered_color.matches)
   document.body.classList.toggle(string_values.DARK_THEME)
 
 button_change_theme.addEventListener('click', handleButtonChangeTheme)
+button_slr_options_visibility.addEventListener('click', handleButtonSlrOptionsVisibility)
+
+Object.entries(game_prperties).forEach(([key, value]) => {
+  const li = document.createElement('li')
+  const button = document.createElement('button')
+
+  button.addEventListener('click', handleButtonSlrOptions)
+  button.setAttribute('key', key)
+  button.innerText = `${value.text} (${value.mines} Mines)`
+
+  li.appendChild(button)
+  slr_options.appendChild(li)
+})
